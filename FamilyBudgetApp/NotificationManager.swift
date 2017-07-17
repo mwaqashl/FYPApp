58288c9b098ea1c92047930d21b4878b1d962327
Waqas Hussain <waqas@technollage.com>
2017-07-17 15:34:48 -0700

    Notifications working..

--- FamilyBudgetApp/NotificationManager.swift
+++ FamilyBudgetApp/NotificationManager.swift
@@ -1,33 +1,21 @@
 
 import Foundation
 import Firebase
+import Alamofire
 
-/*
- 
- Notification Comments:
- 
- Modules 
- 
- - "Wallet"
-        Types:
-            - StatusChanged
-            - OwnerChanged
- 
- - "Budget"
-        Types:
-            - StatusChanged
-            - OwnerChanged
- 
- - "Task"
-        Types:
-            - AddTask
-            - StatusChanged
- 
- - "Transaction"
- - "User"
- 
-
- */
+enum NotificationType {
+    case walletClosed
+    case walletOpen
+    case addedToWallet
+    case removeFromWallet
+    case transactionAdded
+    case messageRecieved
+    case budgetAdded
+    case budgetOverflow
+    case budgetClosed
+    case taskAssigned
+    case taskCompleted
+}
 
 class NotificationManager {
     
@@ -39,22 +27,100 @@ class NotificationManager {
     }
     
     
-    func addNewNotification(_ notification: Notification) {
+//    func addNewNotification(_ notification: Notification) {
+//        
+//        let notRef = ref.child("Notifications").childByAutoId()
+//        
+//        let data : NSMutableDictionary = [
+//            
+//            "module" : notification.module,
+//            "type" : notification.type,
+//            "isPush" : notification.isPush,
+//            "users" : notification.users,
+//            "message" : notification.message,
+//            "details" : notification.details
+//        ]
+//        
+//        notRef.setValue(data)
+//        
+//    }
+    
+    private func sendNotification(toDevicewith deviceID:String, of type: NotificationType, for genericID:String, withCallback callback: @escaping (Bool)->Void) {
+        var title = ""
+        var body = ""
         
-        let notRef = ref.child("Notifications").childByAutoId()
         
-        let data : NSMutableDictionary = [
+        switch type {
+        case .addedToWallet:
+            title = "Wallet Update"
+            body = "You have been added to a wallet."
             
-            "module" : notification.module,
-            "type" : notification.type,
-            "isPush" : notification.isPush,
-            "users" : notification.users,
-            "message" : notification.message,
-            "details" : notification.details
+        case .budgetAdded:
+            title = "Budget Update"
+            body = "A new budget has been added to a wallet."
             
-        ]
+        case .budgetClosed:
+            title = "Budget Closed"
+            body = "A budget you are added in has been updated."
+            
+        case .budgetOverflow:
+            title = "Budget Overflowed"
+            body = "A budget you are added in is overflowed."
+            
+        case .messageRecieved:
+            title = "New Message Recieved"
+            body = "Your Wallet just recieved a new message."
+            
+        case .removeFromWallet:
+            title = "Wallet Update"
+            body = "You have been removed from a wallet."
+            
+        case .taskAssigned:
+            title = "Task Assigned"
+            body = "A task has been assigned to you in your wallet."
+            
+        case .taskCompleted:
+            title = "Task Completed"
+            body = "A task you have been assigned is now completed."
+            
+        case .transactionAdded:
+            title = "Transaction Added"
+            body = "A new transaction has been added to your wallet."
+            
+        case .walletClosed:
+            title = "Wallet Update"
+            body = "One of your wallet is now closed."
+            
+        case .walletOpen:
+            title = "Wallet Update"
+            body = "One of your wallet is now Open."
+            
+        }
         
-        notRef.setValue(data)
+        let params : Parameters = ["APIKey":apiKey,"title":title,"body":body,"genericId":genericID,"bundleId":bundleID,"deviceId":deviceID]
+        
+        Alamofire.request(notificationRequestURL, method: .post, parameters: params).responseJSON { (res) in
+            
+            if res.error != nil {
+                print("Network Error")
+                callback(false)
+                return
+            }
+            
+            guard let data = res.result.value as? Dictionary<String,Any> else {
+                print("Data is not dictionary")
+                callback(false)
+                return
+            }
+            
+            if data["status"] as! String == "Success" {
+                callback(true)
+                return
+            }
+            callback(false)
+            print(data["message"])
+            
+        }
         
     }
     
